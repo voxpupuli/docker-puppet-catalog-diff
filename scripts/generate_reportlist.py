7b4e5f46190b9eeedcf7c34546ba148fc405572b
Raphaël Pinson <raphael.pinson@camptocamp.com>
2016-02-23 21:45:48 +0100

    Put reportlist.json in datadir

--- scripts/generate_reportlist.py
+++ scripts/generate_reportlist.py
@@ -9,5 +9,5 @@ for f in datafiles:
     if f.endswith('.json'):
         f = f[:-5]
         datadict.update({f:f})
-with open(path.join(maindir,'reportlist.json'), 'w') as reportlist:
+with open(path.join(datadir,'reportlist.json'), 'w') as reportlist:
     json.dump(datadict, reportlist, indent=4, sort_keys=True)
