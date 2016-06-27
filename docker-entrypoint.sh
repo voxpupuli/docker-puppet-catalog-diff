a6fc4181926cc75791e9760ada30cc59d6bdf67e
Raphaël Pinson <raphael.pinson@camptocamp.com>
2016-06-27 11:18:56 +0200

    Use docker-entrypoint.d/

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,10 +6,8 @@ DIR=/docker-entrypoint.d
 
 if [[ -d "$DIR" ]]
 then
-  /bin/run-parts "$DIR"
+  /bin/run-parts --verbose --regex '\.sh$' "$DIR"
 fi
-
-configfile="/root/.jobber"
 
 function pipeEnvironmentVariables() {
   local environmentfile="/etc/profile.d/jobber.sh"
@@ -18,44 +16,6 @@ function pipeEnvironmentVariables() {
 _EOF_
   sh -c export >> ${environmentfile}
 }
-
-if [ ! -f "${configfile}" ]; then
-  touch ${configfile}
-  cat > ${configfile} <<_EOF_
----
-_EOF_
-  for (( i = 1; ; i++ ))
-  do
-    VAR_JOB_ON_ERROR="JOB_ON_ERROR$i"
-    VAR_JOB_NAME="JOB_NAME$i"
-    VAR_JOB_COMMAND="JOB_COMMAND$i"
-    VAR_JOB_TIME="JOB_TIME$i"
-
-    if [ ! -n "${!VAR_JOB_NAME}" ]; then
-      break
-    fi
-
-    it_job_on_error="Continue"
-    if [ -n "${!VAR_JOB_ON_ERROR}" ]; then
-      it_job_on_error=${!VAR_JOB_ON_ERROR}
-    fi
-    it_job_name=${!VAR_JOB_NAME}
-    it_job_time=${!VAR_JOB_TIME}
-    it_job_command=${!VAR_JOB_COMMAND}
-
-    cat >> ${configfile} <<_EOF_
-- name: ${it_job_name}
-  cmd: ${it_job_command}
-  time: '${it_job_time}'
-  onError: ${it_job_on_error}
-  notifyOnError: false
-  notifyOnFailure: false
-
-_EOF_
-  done
-fi
-
-cat ${configfile}
 
 if [ "$1" = 'jobberd' ]; then
   pipeEnvironmentVariables
